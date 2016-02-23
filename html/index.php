753861bcfbcd6d7eac5bf05028569162ca53cef9
SirPigles <SirPigles@users.noreply.github.com>
2016-02-23 08:56:05 -0700

    html folder

---
+++ html/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="keywords" contents="Sporkia,wut,shrek,love,life">
+	<meta name="author" content="Sporkia Productions">
+	<title>We Are Sporkia</title>
+	<link href="https://secure.sporkia-engine.com/test/sporkia.css" rel="stylesheet" type="text/css">
+	<script src="//cdn.rawgit.com/namuol/cheet.js/master/cheet.min.js" type="text/javascript"></script>
+	<meta name="google-site-verification" content="SJeVilpacnrFDQlkM8nSBdSkBSmfDzTIf-uIQd880rI" />
+</head>
+<body>
+<div class="content">
+	<div class="title">
+		<img src="https://secure.sporkia-engine.com/logo.jpg" alt="Logo">
+		<?php
+			//Create array of quotes
+			$quotes = array("For old time's sake, please go butt sex that statue.", "Teach me to sexy face.", "I will take drugs anally if you pay me.", "I can teach you to find someone's nipples.", "I am all the ethnicities at once, that makes me...JESUS.", "Everything is arbitrary.", "lol u smell", "I would make and amazing black person and you know it.");
+			//print a random one inside of <p> tags
+			echo '<p>"' . $quotes[array_rand($quotes)] . '"</p>';
+			//end of php
+		?>
+	</div>
+	<div class="butts">
+		<a href="http://wiki.sporkia.com" style="display:block;text-decoration:none;color:#FAFAF5;">
+			<div class="buttonW">
+				<br/>
+				<strong>Sporkia Wiki</strong>
+			</div>
+		</a>
+		<a href="http://sporkia-engine.com" style="display:block;text-decoration:none;color:#FAFAF5;">
+			<div class="buttonE">
+				<br/>
+			     <strong>Sporkia Engine</strong>
+			</div>
+		</a>
+	</div>
+<p>&copy; Sporkia Productions, 2015.</p>
+</div>
+<script>
+	cheet('↑ ↑ ↓ ↓ ← → ← → b a', function () {
+	  window.location = "http://sporkia.com/1.html";
+	});
+</script>
+</body>
+</html>
